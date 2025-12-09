f821bfd2dfffa6e7902030a1823824838f6e2c8b
j1-dev <aephyen@gmail.com>
2025-12-09 18:11:17 +0100

    Nuevo ejercicio

---
+++ ej4.py
@@ -0,0 +1,6 @@
+# Hacer scrapping the https://eurogamer.es
+# TIENE TRUCO
+# Obtener un CSV con fecha, titular y tipo de contenido
+# Obtener 50 noticias
+# Revisar bien el contenido y el HTML de la página
+# Ser responsable (aplicar delay entre peticiones, no petar demasiado)
